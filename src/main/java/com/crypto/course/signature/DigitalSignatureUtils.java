82707747c23a22e39c2ae727bc843c44098174ce
EPAM\Aliaksei_Astapenka1 <aliaksei_astapenka1@epam.com>
2023-05-15 20:35:36 +0200

    add digital signature

---
+++ src/main/java/com/crypto/course/signature/DigitalSignatureUtils.java
@@ -0,0 +1,27 @@
+package com.crypto.course.signature;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+public class DigitalSignatureUtils {
+    private static final String SIGNING_ALGORITHM = "SHA256withRSA";
+
+    private DigitalSignatureUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static byte[] createDigitalSignature(byte[] input, PrivateKey privateKey) throws Exception{
+        Signature signature = Signature.getInstance(SIGNING_ALGORITHM);
+        signature.initSign(privateKey);
+        signature.update(input);
+        return signature.sign();
+    }
+
+    public static boolean verifyDigitalSignature(byte[] input, byte[] signatureToVerify, PublicKey publicKey) throws Exception {
+        Signature signature = Signature.getInstance(SIGNING_ALGORITHM);
+        signature.initVerify(publicKey);
+        signature.update(input);
+        return signature.verify(signatureToVerify);
+    }
+}
